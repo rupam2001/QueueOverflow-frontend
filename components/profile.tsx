56ff4e63141125bcaa6a9d45802ab48fe4da2a7a
rupam2001 <rjdasgalexy@gmail.com>
2022-12-23 23:37:41 +0530

    custom auth login done

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,150 +1,187 @@
-import { useContext, useEffect, useRef, useState } from 'react'
-import { AuthContext } from '../context/authcontext'
-import style from '../styles/Layout.module.css'
-import { AutoAuthProgressImg, NotLoginImg } from '../utils/constanse'
-import { Button } from './stateless/stateless'
-import GoogleLogin from 'react-google-login';
-import { ToggleTheme } from '../utils/helpers'
+import { useContext, useEffect, useRef, useState } from "react";
+import { AuthContext } from "../context/authcontext";
+import style from "../styles/Layout.module.css";
+import { AutoAuthProgressImg, NotLoginImg } from "../utils/constanse";
+import { Button } from "./stateless/stateless";
+import GoogleLogin from "react-google-login";
+import { ToggleTheme } from "../utils/helpers";
 
-import router from 'next/router'
-
-
+import router from "next/router";
 
 export default function Profile() {
-    const authContext = useContext(AuthContext)
-    const ref = useRef(null)
-    const googleSigninButtonRef = useRef(null)
-    const [authInProgress, setAuthInProgress] = useState(true)
+  const authContext = useContext(AuthContext);
+  const ref = useRef(null);
+  const googleSigninButtonRef = useRef(null);
+  const [authInProgress, setAuthInProgress] = useState(true);
 
-    const [isLogin, setIsLogin] = useState(authContext.isLogin)
-    const [profile_pic, setProfile_pic] = useState('')
+  const [isLogin, setIsLogin] = useState(authContext.isLogin);
+  const [profile_pic, setProfile_pic] = useState("");
 
-    useEffect(() => {
-        setIsLogin(authContext.isLogin)
-        setProfile_pic(authContext.AuthRespObj.profile_pic)
-    }, [authContext])
+  useEffect(() => {
+    setIsLogin(authContext.isLogin);
+    setProfile_pic(authContext.AuthRespObj.profile_pic);
+  }, [authContext]);
 
-    const handleOpen = (e) => {
-        //to open the main menu body
-        if (ref.current.style.display === 'none') {
-            //open
-            ref.current.style.display = 'block'
-
-        } else {
-            ref.current.style.display = 'none'
-        }
-
+  const handleOpen = (e) => {
+    //to open the main menu body
+    if (ref.current.style.display === "none") {
+      //open
+      ref.current.style.display = "block";
+    } else {
+      ref.current.style.display = "none";
     }
-    useEffect(() => {
-        //for enter press search
-        const fn = (e) => {
-            if (e.target.id !== 'myDropdown' && !e.target.classList.contains('dd-item') && e.target.id !== 'ignore') {
-                // alert("i")
-                console.log(e.target)
-                ref.current.style.display = 'none'
-            }
-        }
-        window.addEventListener("mousedown", fn)
-        return () => {
-            window.removeEventListener("mousedown", fn)
-        }
-    }, [])
+  };
+  useEffect(() => {
+    //for enter press search
+    const fn = (e) => {
+      if (
+        e.target.id !== "myDropdown" &&
+        !e.target.classList.contains("dd-item") &&
+        e.target.id !== "ignore"
+      ) {
+        // alert("i")
+        console.log(e.target);
+        ref.current.style.display = "none";
+      }
+    };
+    window.addEventListener("mousedown", fn);
+    return () => {
+      window.removeEventListener("mousedown", fn);
+    };
+  }, []);
 
+  useEffect(() => {
+    (async () => {
+      if (!authContext.isLogin) await authContext.AutoAuthFromTokenAsync();
+      setAuthInProgress(false);
+    })();
+  }, []);
 
+  const handleSignin = () => {
+    googleSigninButtonRef.current.click();
+  };
 
-    useEffect(() => {
-        (
-            async () => {
-                if (!authContext.isLogin)
-                    await authContext.AutoAuthFromTokenAsync()
-                setAuthInProgress(false)
-            }
-        )()
+  const responseSuccessGoogle = (response) => {
+    authContext.AuthenticateAsync(response.tokenId);
+  };
+  const responseErrorGoogle = (response) => {
+    // alert("Error")
+  };
 
-    }, [])
+  const handleLogout = () => {
+    setAuthInProgress(true);
+    authContext.LogoutAsync().then((success) => {
+      if (success) {
+        setAuthInProgress(false);
+      } else {
+        alert("failed to logout :(");
+      }
+    });
+  };
+  const handleThemeClick = () => {
+    ToggleTheme();
+  };
 
-    const handleSignin = () => {
-        googleSigninButtonRef.current.click()
+  const handleMyQuestionClick = () => {
+    router.push("/myquestions");
+  };
+
+  const [currTheme, setCurrTheme] = useState(null);
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    setCurrTheme(theme);
+    // alert(theme)
+  }, []);
+
+  const handleMyArticleClick = () => {
+    router.push("/myarticles");
+  };
+  const onClickSignin = () => {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  };
+  const onClickLogin = async () => {
+    const email = (document.getElementById("signin_email") as HTMLInputElement)
+      .value;
+    const password = (
+      document.getElementById("signin_pass") as HTMLInputElement
+    ).value;
+    const resp = await authContext.AuthenticateAsync2(email, password);
+    const { success } = resp;
+    if (success) {
+      var modal = document.getElementById("myModal");
+      modal.style.display = "none";
+    } else {
+      alert("Wrong credentials");
     }
+  };
 
-    const responseSuccessGoogle = (response) => {
-        authContext.AuthenticateAsync(response.tokenId)
-    }
-    const responseErrorGoogle = (response) => {
-        // alert("Error")
-    }
-
-    const handleLogout = () => {
-        setAuthInProgress(true);
-        authContext.LogoutAsync()
-            .then(success => {
-                if (success) {
-                    setAuthInProgress(false)
-                } else {
-                    alert("failed to logout :(")
-                }
-            })
-    }
-    const handleThemeClick = () => {
-        ToggleTheme()
-    }
-
-    const handleMyQuestionClick = () => {
-        router.push("/myquestions")
-    }
-
-    const [currTheme, setCurrTheme] = useState(null)
-    useEffect(() => {
-        const theme = localStorage.getItem('theme')
-        setCurrTheme(theme)
-        // alert(theme)
-    }, [])
-
-    const handleMyArticleClick = () => {
-        router.push('/myarticles')
-    }
-
-
-    return (
-        <div className="dropdown" >
-            {
-                isLogin ? (
-                    <img className={style.profile} src={profile_pic} onClick={handleOpen} />
-                ) : (
-                        <>
-                            { !authInProgress && <Button onclickCallBack={handleOpen} text="Signin" buttonStyle={{ backgroundColor: 'transparent', color: "green", fontWeight: 'bolder' }} />}
-                            {authInProgress && <img className={style.profile} src={AutoAuthProgressImg} style={{ border: '1px solid grey' }} />}
-                        </>
-                    )
-            }
-            <div className="dropdown-content" style={{ right: 0 }} ref={ref} id="myDropdown">
-                {
-                    isLogin ? (
-                        <>
-                            <div className="dd-item" id="myDropdown" onClick={handleMyQuestionClick}>
-                                <i className={'fa fa-clipboard'} aria-hidden="true"></i>
-                                <span id="ignore">My questions</span>
-                            </div>
-                            <div className="dd-item" id="myDropdown" onClick={handleMyArticleClick}>
-                                <i className={'fa fa-pencil-square-o'} aria-hidden="true"></i>
-                                <span id="ignore">My Articles</span>
-                            </div>
-                            <div className="dd-item" id="myDropdown" onClick={handleThemeClick}>
-                                <i className={"fa fa-paint-brush "} aria-hidden="true"></i>
-                                <span id="ignore">Theme</span>
-                            </div>
-                            <div className="dd-item" id="myDropdown" onClick={handleLogout}>
-                                <i className="fa fa-sign-out" aria-hidden="true"></i>
-                                <span id="ignore">Logout</span>
-                            </div>
-                        </>
-                    ) : (
-                            <div id="myDropdown">
-                                <div className="dd-item" id="myDropdown" onClick={handleThemeClick}>
-                                    <i className={"fa fa-paint-brush"} aria-hidden="true"></i>
-                                    <span id="ignore">Theme</span>
-                                </div>
-                                <GoogleLogin
+  return (
+    <div className="dropdown">
+      {isLogin ? (
+        <img className={style.profile} src={profile_pic} onClick={handleOpen} />
+      ) : (
+        <>
+          {!authInProgress && (
+            <Button
+              onclickCallBack={handleOpen}
+              text="Signin"
+              buttonStyle={{
+                backgroundColor: "transparent",
+                color: "green",
+                fontWeight: "bolder",
+              }}
+            />
+          )}
+          {authInProgress && (
+            <img
+              className={style.profile}
+              src={AutoAuthProgressImg}
+              style={{ border: "1px solid grey" }}
+            />
+          )}
+        </>
+      )}
+      <div
+        className="dropdown-content"
+        style={{ right: 0 }}
+        ref={ref}
+        id="myDropdown"
+      >
+        {isLogin ? (
+          <>
+            <div
+              className="dd-item"
+              id="myDropdown"
+              onClick={handleMyQuestionClick}
+            >
+              <i className={"fa fa-clipboard"} aria-hidden="true"></i>
+              <span id="ignore">My questions</span>
+            </div>
+            <div
+              className="dd-item"
+              id="myDropdown"
+              onClick={handleMyArticleClick}
+            >
+              <i className={"fa fa-pencil-square-o"} aria-hidden="true"></i>
+              <span id="ignore">My Articles</span>
+            </div>
+            <div className="dd-item" id="myDropdown" onClick={handleThemeClick}>
+              <i className={"fa fa-paint-brush "} aria-hidden="true"></i>
+              <span id="ignore">Theme</span>
+            </div>
+            <div className="dd-item" id="myDropdown" onClick={handleLogout}>
+              <i className="fa fa-sign-out" aria-hidden="true"></i>
+              <span id="ignore">Logout</span>
+            </div>
+          </>
+        ) : (
+          <div id="myDropdown">
+            <div className="dd-item" id="myDropdown" onClick={handleThemeClick}>
+              <i className={"fa fa-paint-brush"} aria-hidden="true"></i>
+              <span id="ignore">Theme</span>
+            </div>
+            {/* <GoogleLogin
                                     clientId="432827620544-earre7sba34jptupkvjuinarabmts09e.apps.googleusercontent.com"
                                     buttonText="Login"
                                     onSuccess={responseSuccessGoogle}
@@ -160,12 +197,53 @@ export default function Profile() {
                                         </div>
 
                                     )}
-                                />
-                            </div>
-                        )
-                }
+                                /> */}
+            <div
+              className="dd-item"
+              id="myDropdown"
+              onClick={() => {
+                onClickSignin();
+              }}
+              ref={googleSigninButtonRef}
+            >
+              <i className="fa fa-google" aria-hidden="true"></i>
+              <span id="ignore"> Signin</span>
             </div>
+          </div>
+        )}
+      </div>
 
+      <div id="myModal" className="modal">
+        <div className="modal-content" style={{}}>
+          <span
+            className="close"
+            onClick={() => {
+              var modal = document.getElementById("myModal");
+              modal.style.display = "none";
+            }}
+          >
+            &times;
+          </span>
+          <div
+            style={{
+              display: "flex",
+
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <input type="text" placeholder="email" id="signin_email" />
+            <input type="password" placeholder="password" id="signin_pass" />
+            <button onClick={onClickLogin}>Login</button>
+          </div>
+          {/* <div style={{ display: "flex" }}>
+            <input type="text" placeholder="name" />
+            <input type="text" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button>Signup</button>
+          </div> */}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
